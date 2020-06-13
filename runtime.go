862f147a98222fc1781d46726625c0063ad2922a
sahancava <sahancava@marun.edu.tr>
2020-06-13 05:54:22 +0300

    a few

--- runtime.go
+++ runtime.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"errors"
 	"fmt"
-	"runtime"
+	"math"
 )
 
 func main() {
+	result, err := sqrt(-1)
 
-	fmt.Println(runtime.GOOS)
-	fmt.Println(runtime.GOARCH)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(result)
+	}
+}
 
+func sqrt(param float64) (float64, error) {
+	if param < 0 {
+		return 0, errors.New("Undefined integer value. Should be higher than 0 value.")
+	}
+	return math.Sqrt(param), nil
 }
