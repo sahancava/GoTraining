ab1ac40283a84c44177ed10aa0efe4e050141748
sahancava <sahancava@marun.edu.tr>
2020-06-28 14:53:36 +0300

    .env file integration

--- packages/connector/connector.go
+++ packages/connector/connector.go
@@ -2,21 +2,14 @@ package connector
 
 import (
 	"database/sql"
-	"fmt"
-)
-
-const (
-	dbhost = "localhost"
-	dbport = 5432
-	dbuser = "postgres"
-	dbpass = "Asdf1234"
-	dbname = "dvdrental"
+	"github.com/joho/godotenv"
+	"os"
 )
 
 func ConnectDB() *sql.DB {
 	var err error
-	psqlInfo := fmt.Sprintf("postgres://%s:%s@%s:%d/%s", dbuser, dbpass, dbhost, dbport, dbname)
-
+	err = godotenv.Load(".env")
+	psqlInfo := os.ExpandEnv("$POSTGRES_URL")
 	db, err := sql.Open("postgres", psqlInfo)
 	if err != nil {
 		panic(err)
