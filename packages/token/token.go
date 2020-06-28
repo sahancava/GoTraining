c30617d3a05f96a357380dfc2641401599e0bae4
sahancava <sahancava@marun.edu.tr>
2020-06-28 03:00:14 +0300

    English Translation

--- packages/token/token.go
+++ packages/token/token.go
@@ -9,7 +9,7 @@ func TokenChecker(w http.ResponseWriter, req *http.Request) bool {
 	key := req.Header.Get("token")
 	switch {
 	case key != "123123123":
-		fmt.Fprintf(w, "API Key Yanlış")
+		fmt.Fprintf(w, "API Key is not correct.")
 		return false
 	}
 	return true
